1cb63e54f99994929bd1780814e5ba3235ebcea4
gerry <gerrydesrian488@gmail.com>
2024-10-21 21:12:05 +0700

    update error message

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -167,7 +167,7 @@ const loginAsGuest = async () => {
 							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 						/>
 					</svg>
-					<span>Please fill in all required fields</span>
+					<span>Please fill in all required fields </span>
 				</div>
 			</div>
 			<div class="card-body max-sm:w-[44vh] backdrop-blur-sm mt-10">
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -145,7 +145,7 @@ const setAvatar = (id: number) => {
 									d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
 								/>
 							</svg>
-							<span>Please fill in all required fields</span>
+							<span>Please fill in all required fields </span>
 						</div>
 					</div>
 					<h2 class="text-2xl font-semibold text-white">Register</h2>
